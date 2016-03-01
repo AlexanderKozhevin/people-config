ebfd920d501dbfc3f998c00f83373b015a0a8069
root <info@alexander-kozhevin.com>
2016-03-01 18:39:47 +0300

    activation script

--- activate_user.js
+++ activate_user.js
@@ -1,2 +1,19 @@
 var yargs = require('yargs').argv;
-console.log(yargs.user)
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/sails');
+
+var Schema = mongoose.Schema;
+var User = new Schema({
+	email: {type: "string"},
+	pass: {type: "string"},
+	active: {type: "boolean"}
+});
+var Users = mongoose.model('users', User);
+Users.findOne({email: yargs.email}, function (err, user) {
+  user.active = true;
+  user.save() 
+  console.log('activated');
+  process.exit();
+});
+
